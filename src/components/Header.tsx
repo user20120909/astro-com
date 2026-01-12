8ef02f31f0fa9e2ffdbb77cfb839861d2a55a265
banaan <grjochem2@gmail.com>
2026-01-12 18:23:23 +0100

    minor change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC<Props> = ({ shopUrl }) => {
                 className="h-10 w-auto sm:h-12"
               />
               <span className="ml-3 text-2xl sm:text-3xl font-bold">
-                Gurls Life
+                gurls life
               </span>
             </a>
           </div>
